4fe2f46592111f458422214876093eb6e6224136
MJ992399 <Mariamjalaghonia8@gmail.com>
2024-09-23 17:46:27 +0400

    second commit

---
+++ day 04/homework.py
@@ -0,0 +1,26 @@
+#დავალება: მომხმარებელს შემოატანინეთ 2 რიცხვი, ხოლო შემდეგ გაკეთეთ ამ რიცხვებზე ყველა მათემატიკური ოპერაცია ცალცალკე.
+
+
+weight=int(input("please enter your weight:"))
+
+height=int(input("please enter your height: "))
+
+
+
+print(height+weight)
+print(height-weight)
+print(height*weight)
+print(height / weight)
+
+
+#დავალება: კომენტარიბის სახით ახსენით თქვენი სიტყვებით, რა არის input, და output
+
+
+# An input is an information that goes to computer 
+# An output is a way for the computer to communicate with the outside world. 
+# A message displayed on the screen and the sound from a speaker are examples of outputs.
+
+#დავალება: მომხმარებელს შემოატანინეთ თავისი სახელი და გავარი, შემდეგ კი ტერმინალში გამოიტანეთ მომხმარებლის შემოტანილი მნიშვნელობები
+
+name=input("enter your name: ")
+surname=input("enter your surname: ")
