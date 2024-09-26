85cc117c8d788492e8869d37b9e9bbb4cc2305c7
MJ992399 <Mariamjalaghonia8@gmail.com>
2024-09-26 15:10:34 +0400

    second commit

---
+++ day 05/homework.py
@@ -0,0 +1,50 @@
+
+#6) მომხმარებელს input ის საშუალებით შემოატანინეთ სახელი შემდეგ კი პროგრამა მას მიესალმოს 
+name=(input("enter your name:"))
+print("hello" + name)
+
+
+#7) მომხმარებელს შემოატანინეთ სახელი შემდეგ გვარი და ტერმინალში დაბეჭდეთ მისი სრული სახელი და გვარი მოახდინეთ მათი კონკატინაცია 
+
+name=(input("please enter your name: "))
+surname=(input("please enter your surname: "))
+
+print(name + surname )
+
+
+
+
+
+#მომხმარებელს შემოატანინეთ რიცხვი შემდეგ კი დაბეჭდეთ მომხმარებლის რიცხვს დამატებული 5
+
+num=int(input("eter number:"))
+
+print( num + 5)
+
+#მომხმარებელს შემოატანინეთ რიცხვი შემდეგ კი დაბეჭდეთ ამ რიცხვის კვადრატი
+
+num=int(input("enter number:"))
+
+square = num* num 
+
+print(square )
+
+#მომხმარებელს შემოატანინეთ ორი რიცხვი შემდეგ კი ამ ორი რიცხვისგან შეადგინეთ ოთხი მათემატიკური მოქმედება, დამატება გამოკლება გამრავლება და გაყოფა
+
+
+height=int(input("enter height:"))
+weight=int(input("enter weight: "))
+
+
+print(weight+ height)
+print(height- weight)
+print(height * weight)
+print (height/ weight )
+
+
+# 12) მომხმარებელს შეეკითხეთ მისი ასაკი შემდეგ კი დაბეჭდეთ 10 წლის შემდეგ რამდენი წლის იქნება
+
+age=int(input(" how old are you: "))
+
+
+print(age  + 10 )
