98ff11d68c02865444903c1a5837c1e59276da28
MJ992399 <Mariamjalaghonia8@gmail.com>
2024-09-27 20:38:20 +0400

    second commit

---
+++ day 07/homework.py/homework.py/homework.py
@@ -0,0 +1,59 @@
+
+
+    #1) შექმენი ორი input შეადარე ორი რიცხვი და დაბეჭდე შედეგი.
+ 
+
+
+height=int(input("enter height:"))
+weight=int(input("enter weight: "))
+
+print(height > weight )
+
+     #2) შექმენი 2 ცვლადი შეამოწმე, ტოლია თუ არა ორი რიცხვი. 
+num1=56
+num2=4374
+
+print(num1 == num2 )
+
+
+#3) მომხმარებელს შემოახანინე input შეამოწმე, არის თუ არა რიცხვი 50-ზე ნაკლები.
+
+monthly_salary=int(input("enter your monthly salary:" ))
+
+print(monthly_salary < 50 )
+
+
+#4)შემოიტანე 2 input შეამოწმე, არის თუ არა ორი რიცხვის ჯამი 100-ზე მეტი.
+
+num5=int(input("enter number: "))
+num6=int(input("enter number 2:"))
+
+print(num5+num6)
+
+result=128
+
+print(result>100 )
+
+    #5) მომხარებელს შემოატანინე input შეამოწმე, არის თუ არა რიცხვი 10-ზე მეტი ან ტოლი.
+
+num1=int(input("enter number"))
+
+print(num1>= 10)
+
+#6)შემოიტანე 1 input და 1 ცლადი შეადარე, არის თუ არა ერთი რიცხვი მეორეზე ნაკლები ან ტოლი.
+
+num1=int(input("enter number: "))
+num2=45
+print(num1>=num2)
+
+#7) მომხმარებელს შემოატანინეთ სახელი და შეამოწმეთ ეს სახელი უდრის თუ არა თქვენს სახელს
+
+name=(input("enter your name:"))
+my_name=("mariami")
+print(my_name==name)
+
+ #8) მომხმარებელს შემოატანინეთ პაროლი შემდეგ კითხეთ ხელახლა რომ გაიმეოროს პაროლი და შეამოწმეთ უდრის თუ არა ისინი ერთმანეთს
+
+password=(input("enter your password:"))
+password2=(input("enter password 2"))
+print(password == password2)
