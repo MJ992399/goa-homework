8c1c2aae7362aec722e0c5868c4d02a59926b168
MJ992399 <Mariamjalaghonia8@gmail.com>
2024-10-12 13:18:43 +0400

    second commit

---
+++ day 12/classwork/classwork.py
@@ -0,0 +1,20 @@
+#for loop
+#while loop
+
+
+#loop  ლუპი გრძელდება უსასრულოდ
+
+#cikli ციკლს აქვს განსაზღვრული რაოდენობა რამდენსაც შენ მიუთითებ
+
+#In general, use for loops when you already know the number of iterations, and while loops when there is a condition that needs to be met.
+# While loops are powerful because they can be used even when you don’t know how many iterations will be needed.
+# you can apply iteration to your programs with the while loop
+#  counters keep track of the number of iterations and avoid infinite loops
+# indentation and the colon : symbol are required for the code to run
+#interation- iteracia-ganmeoreba
+#i- საიტერაციო ცვლადი
+
+for i in range(20):
+    print("goa best")
+
+
