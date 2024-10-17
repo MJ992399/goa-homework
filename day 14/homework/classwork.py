baaf50f79b692954951e9a00281c7e10518883e4
MJ992399 <Mariamjalaghonia8@gmail.com>
2024-10-17 16:42:12 +0400

    second commit

---
+++ day 14/homework/classwork.py
@@ -0,0 +1,106 @@
+# For loop 
+#  0-იდან 20-ის ჩათვლით დაპრინტეთ ყველა მთელი რიცხვი
+
+# for i in range(21):
+#     print(i)
+
+
+
+# დაპრინტეთ პირველი 10 ნატურალური რიცხვი 
+
+# for i in range(11):
+#     print(float(i))
+
+
+# დაპტინტეთ 0-იდან 100-ის ჩათვლით ლუწი რიცხვები
+
+
+# for i in range (101):
+#     if i%2==0:
+#         print(i)
+
+# Write a program that calculates and prints the sum of numbers from 1 to 10 using a for loop.
+
+# sum=0
+
+# for i in range(1,11):
+#     sum=sum+i
+# print(sum)
+
+
+# )Print the squares of numbers from 1 to 15.
+
+# for i in range(1,16):
+#     print(i*i)
+
+
+
+# )Write a program that calculates and prints the sum of squares of numbers from 1 to 5 using a for loop.
+ 
+
+# sum = 0
+
+# for i in range(1,10):
+#     sum += i ** 2
+
+# print(sum)
+
+
+
+
+# )Print numbers divisible by both 3 and 5 from 1 to 100 inclusive.
+
+#  for i in range (1,101):
+#      if i%3==0 and i%5==0:
+#      print(i)
+
+
+# )Write a program that prints numbers from 10 to 1 in reverse order using a for loop 
+
+
+# for i in range(11):
+#        print(11-i)
+
+
+# )შემოატანინეთ მომხმარებელს რიცხვი და დაადგინეთ არის თუ არა დადებითი უარყოფითი ან ნულის ტოლი 
+
+# num=int(input("enter number"))
+
+# if num > 0:
+#     print("dadebiti")
+# elif num < 0:
+#     print("uaryofiti")
+# else:
+#     print("equal to zero!")
+
+# )შემოატანინეთ მომხმარებელს მისი ასაკი თუ მისი ასაკი 18 წელზე მეტია დაუპრინტეთ “you are adult” თუ 18 წელზე ნაკლები “you are virgin”
+
+
+# age=int(input("enter your age:"))
+
+# if age>=18:
+#     print("you're an adult")
+# else:
+#     print("you're a virgin")
+    
+# )დაწერეთ ისეთი პროგრამა რომელიც მომხმარებელს უპრინტავს კვირის დღეს მომხმარებლის შემოტანილი რიცხვის მიხედვით (1 არის ორშაბათი, 2 სამშაბათი და ა.შ) გამოიყენეთ if elif else:
+
+
+# num=input("enter any number from 1 to 7:")
+
+# if num=="1":
+#     print("orshabati")
+# elif num=="2":
+#     print("samshabati")
+# elif num=="3":
+#     print("otxshabati")
+# elif num=="4":
+#     print("xutshabati")
+# elif num=="5":
+#     ("paraskevi")
+# elif num=="6":
+#     print("shabati")
+# elif num=="7":
+#     print("kvira")
+# else:
+#     print("invalid number")
